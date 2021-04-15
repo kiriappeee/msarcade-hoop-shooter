e6435f86fc82a04cf5bf8910951747ff92866f4c
Adnan Issadeen <adnan.issadeen@hey.com>
2021-04-15 00:04:50 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+scene.setBackgroundColor(13)
+let mySprite = sprites.create(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f e e e e e e e f f . 
+    f f e f e e e e e e f f 
+    f f f e e e e e e e e f 
+    f f f e e e e e e f e f 
+    f f f f e e e e f f f f 
+    f f f f f f f f f f f f 
+    f f f f f f f f f f f f 
+    . f f f f f f f f f f . 
+    . e f f f f f f f f e . 
+    e 4 f b b b b b b f 4 e 
+    4 d f d d d d d d c d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
